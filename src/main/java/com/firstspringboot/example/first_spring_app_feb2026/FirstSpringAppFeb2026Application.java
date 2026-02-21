0e4b55d85723db376b061c52adfdd04a1be622f8
Chandan K M <chandanchintu16@gmail.com>
2026-02-21 14:02:22 +0530

    first spring boot app created and understood structure

---
+++ src/main/java/com/firstspringboot/example/first_spring_app_feb2026/FirstSpringAppFeb2026Application.java
@@ -0,0 +1,14 @@
+package com.firstspringboot.example.first_spring_app_feb2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppFeb2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppFeb2026Application.class, args);
+		System.out.println("Hello all, this is our first spring boot app!!");
+	}
+
+}
---
+++ src/test/java/com/firstspringboot/example/first_spring_app_feb2026/FirstSpringAppFeb2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringboot.example.first_spring_app_feb2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppFeb2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
